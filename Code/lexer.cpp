5b5c9b98354ff4e2f2420a0493f9bbdf2a3ad91f
Ka4ma <109244581+keyaanminhas@users.noreply.github.com>
2023-10-30 02:26:09 +0500

    Add files via upload

---
+++ Code/lexer.cpp
@@ -0,0 +1,100 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+#include <tuple>
+#include "lexer.h"
+
+
+TOKEN_TYPE TOKEN_LIST;
+
+std::string tokenizer(TOKEN_TYPE& TOKEN_LIST, const std::string& RAW_CODE){
+    std::string NEXT;
+    std::string CURRENT;
+    int LINE_NUMBER = 1;
+    std::string report;
+    std::string::size_type code_size = RAW_CODE.size();
+    bool string_toggle = false;
+    int i = 0;
+    int j = 0;
+    while (i != code_size && j <= code_size){
+        while (RAW_CODE[i] != ' ' || string_toggle == true && i!= code_size ){
+            if (RAW_CODE[i] == '"'){
+                string_toggle = !string_toggle;
+            }
+            i++;
+        }
+        CURRENT = RAW_CODE.substr(j, i-j);
+        int linechange = CURRENT.find(R"(\n)");
+        if ( linechange != std::string::npos){
+            
+            NEXT = CURRENT.substr(linechange + 2, CURRENT.size() - linechange);
+            CURRENT = CURRENT.substr(0, linechange); 
+
+            categorize(TOKEN_LIST, CURRENT, LINE_NUMBER);
+            LINE_NUMBER ++;
+            categorize(TOKEN_LIST, NEXT, LINE_NUMBER);
+        }
+        else{
+            categorize(TOKEN_LIST, CURRENT, LINE_NUMBER);
+        }
+
+        ++i;
+        j = i;
+    }
+
+    return report;
+};
+
+bool is_keyword(const std::string& word){
+    for(int i = 0; i != KEYWORDS_SIZE; i++){
+        if (word == KEYWORDS[i]){
+            return true;
+        }
+    }
+    return false;
+};
+
+bool is_operator(const std::string& word){
+    for(int i = 0; i != OPERATORS_SIZE; i++){
+        if (word == OPERATORS[i]){
+            return true;
+        }
+    }
+    return false;
+};
+
+
+bool is_number(const std::string& word)
+{
+    std::string::const_iterator it = word.begin();
+    while (it != word.end() && (std::isdigit(*it) || *it == '.')) ++it;
+    return !word.empty() && it == word.end();
+}
+
+void categorize(TOKEN_TYPE& TOKEN_LIST, const std::string& CURRENT, const int& LINE_NUMBER){
+    if (is_keyword(CURRENT)){
+        TOKEN_LIST.push_back(std::tuple<int, std::string, std::string> {LINE_NUMBER, "KW", CURRENT});
+    }
+    else if(is_operator(CURRENT)){
+        TOKEN_LIST.push_back(std::tuple<int, std::string, std::string> {LINE_NUMBER, "OP", CURRENT});
+    }
+    else if (CURRENT[0] == '"' && CURRENT[CURRENT.size()-1] == '"'){
+        TOKEN_LIST.push_back(std::tuple<int, std::string, std::string> (LINE_NUMBER, "STR", CURRENT));
+    }
+    else if (is_number(CURRENT)){
+        TOKEN_LIST.push_back(std::tuple<int, std::string, std::string> (LINE_NUMBER, "NUM", CURRENT));
+    }
+    else{
+        TOKEN_LIST.push_back(std::tuple<int, std::string, std::string> (LINE_NUMBER, "ID", CURRENT));
+    }
+}
+
+
+int main(){
+    std::string CODE = R"(DECLARE NUM : INTEGER\nDECLARE NAME : STRING\nNUM = 3.2\nNAME <-- "Keyaan Minhas")";
+    tokenizer(TOKEN_LIST, CODE);
+    for (int i= 0; i != TOKEN_LIST.size(); i++){
+        std::cout << std::get<0>(TOKEN_LIST[i]) << " " << std::get<1>(TOKEN_LIST[i]) << " " << std::get<2>(TOKEN_LIST[i]) << std::endl;
+    }
+}
---
+++ Code/lexer.h
@@ -0,0 +1,32 @@
+#ifndef LEXER_H
+#define LEXER_H
+#include <string>
+#include <vector>
+#include <iostream>
+#include <tuple>
+
+
+
+typedef std::vector<std::tuple<int, std::string, std::string>> TOKEN_TYPE;
+
+// LATER UPDATES SHALL INCLUDE 
+// - STRUCTS
+// - CONSTANTS
+// - FUNCTIONS
+// - PROCEDURES
+
+const std::vector<std::string> KEYWORDS{"INTEGER", "REAL", "STRING", "BOOLEAN", "CHAR", "DECLARE", "ARRAY", "OF", "INPUT", "OUTPUT", "IF", "THEN", "ELSE", "ENDIF", "CASE", "OTHERWISE", "ENDCASE", "FOR", "TO", "ENDFOR", "REPEAT", "UNTIL", "ENDWHILE", "WHILE"};
+const std::vector<std::string> OPERATORS{":","+", "-", "=", "<--", "<", ">", "<=", ">=", "<>", "AND", "OR", "NOT"};
+
+const std::vector<std::string>::size_type KEYWORDS_SIZE = KEYWORDS.size();
+const std::vector<std::string>::size_type OPERATORS_SIZE = OPERATORS.size();
+
+
+
+
+std::string tokenizer(TOKEN_TYPE& TOKEN_LIST, const std::string& RAW_CODE);
+bool is_keyword(const std::string& word);
+bool is_operator(const std::string& word);
+bool is_number(const std::string& word);
+void categorize(TOKEN_TYPE& TOKEN_LIST, const std::string& CURRENT, const int& LINE_NUMBER);
+#endif
